01087c1897288d0310d3efb32ac35f6c4631f734
Oleg Yakutenok <oleg.y@altoros.com>
2016-06-09 07:34:25 +0300

    Fixing 'request_payment' URL

--- lib/pay_junction/client/smart_terminals.rb
+++ lib/pay_junction/client/smart_terminals.rb
@@ -3,7 +3,7 @@ module PayJunction
     module SmartTerminals
 
       def request_payment(smart_terminal_id, options)
-        post("/smartterminals/#{smart_terminal_id}/request_payment", options)
+        post("/smartterminals/#{smart_terminal_id}/request-payment", options)
       end
 
     end
